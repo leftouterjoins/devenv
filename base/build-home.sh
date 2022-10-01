d0532a15e61afa0906a59e9a0f1337d9c2887750
josh <josh@brickner.dev>
2022-10-01 03:16:19 +0000

    Added env vars for git auth

--- base/build-home.sh
+++ base/build-home.sh
@@ -5,5 +5,11 @@ git config --global user.name $DEVENV_USER
 git config --global user.email $DEVENV_EMAIL
 git config --global core.editor "NVIM_LISTEN_ADDRESS=/home/$DEVENV_USER/nvimsocket nvr --nostart --remote-wait-silent"
 
+git config --global credential.helper store
+
+touch /home/$DEVENV_USER/.git-credentials
+echo "https://$DEVENV_GH_USER:$DEVENV_GH_PASS@github.com" >> /home/$DEVENV_USER/.git-credentials
+echo "https://$DEVENV_SH_USER:$DEVENV_SH_PASS@git.brickner.cloud" >> /home/$DEVENV_USER/.git-credentials
+
 ~/setup.sh
 
